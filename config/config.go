a4ee1d02d629dba502a2657901b88f0d2e1fe79c
George <gosha23321@gmail.com>
2025-05-28 14:39:53 +0200

    change cfg

--- config/config.go
+++ config/config.go
@@ -20,13 +20,17 @@ type Config struct {
 }
 
 func New() (*Config, error) {
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
 	viper.SetConfigFile(pathConfigFile)
 	viper.SetConfigType(configType)
 
-	
-
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-	viper.AutomaticEnv()
+	viper.BindEnv("blockchain.url", "BLOCKCHAIN_URL")
+	viper.BindEnv("postgres.host", "POSTGRES_HOST")
+	viper.BindEnv("postgres.user", "POSTGRES_USER")
+	viper.BindEnv("postgres.password", "POSTGRES_PASSWORD")
+	viper.BindEnv("postgres.db", "POSTGRES_DB")
+	viper.BindEnv("postgres.ssl_mode", "POSTGRES_SSLMODE")
 
 	if err := viper.ReadInConfig(); err != nil {
 		return nil, err

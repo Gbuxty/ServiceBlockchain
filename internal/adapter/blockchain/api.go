72044a80fe0d28ea0327debbf37eb716c0dd8e45
George <gosha23321@gmail.com>
2025-05-28 14:42:30 +0200

    add log url

--- internal/adapter/blockchain/api.go
+++ internal/adapter/blockchain/api.go
@@ -3,6 +3,7 @@ package bc
 import (
 	"BlockchainCurrency/config"
 	"BlockchainCurrency/internal/domain"
+	"BlockchainCurrency/pkg/logger"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -12,12 +13,14 @@ import (
 type Blockchain struct {
 	cfg    *config.Config
 	client *http.Client
+	logger logger.Logger
 }
 
-func NewBclockchain(cfg *config.Config) *Blockchain {
+func NewBclockchain(cfg *config.Config,logger logger.Logger) *Blockchain {
 	return &Blockchain{
 		cfg:    cfg,
 		client: &http.Client{Timeout: cfg.HTTP.ReadTimeout},
+		logger: logger,
 	}
 }
 
@@ -30,6 +33,8 @@ type DataBlockchainResponse struct {
 
 func (s *Blockchain) FetchQuotesData(ctx context.Context, symbol string) (domain.Quotes, error) {
 	url := fmt.Sprintf("%s/%s", s.cfg.BlockchainUrl.URL, symbol)
+	s.logger.Infof("Requesting quote: %s", url)
+	
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		return domain.Quotes{}, fmt.Errorf("failed to create request for %s: %v", symbol, err)
@@ -39,7 +44,7 @@ func (s *Blockchain) FetchQuotesData(ctx context.Context, symbol string) (domain
 	if err != nil {
 		return domain.Quotes{}, fmt.Errorf("request failed for %s: %v", symbol, err)
 	}
-
+	
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
